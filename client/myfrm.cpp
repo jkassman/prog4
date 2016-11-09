c633f90d02a07cead540218d28b30201d0e8e0e7
lkuta <lkuta@nd.edu>
2016-11-09 17:38:31 -0500

    Username stuff.

--- client/myfrm.cpp
+++ client/myfrm.cpp
@@ -1,3 +1,11 @@
+/*
+  Authors: Jacob Kassman, Lauren Kuta, Matt Paulson
+  netIDs: jkassman, lkuta, mpaulson
+  Computer Networks: Programming Assignment 4
+  myfrm.cpp
+  Creates a client to access a forum server.
+ */
+
 #include <iostream>
 
 #include "../common.hpp"
@@ -52,6 +60,39 @@ int main(int argc, char **argv)
     tcpRecv(tcpSock, buffy, 1000, "error receiving test message");
     cout << buffy;
     close(tcpSock);
+ 
+    //Client sends the username:
+    /* 
+     string username;
+     char userResponseBuff[1000];
+     char userResponseBuff2[1000];
+     cout << "Please enter your desired username: " << endl;
+     cin >> username;
+     int len = strlen(username.c_str())+1;
+     udpSend(udpSock, username.c_str(), len, (struct sockaddr*)&udpSock, sizeof(struct sockaddr), "Could not send username");
+
+    //Receives call from server determining whether the username has been taken or not:
+     udpRecv(udpSock, userResponseBuff, 999, (struct sockaddr*)&client_socket, &addr_len, "Could not receive username confirmation");
+     cout << userResponseBuff << endl;
+
+    //If ok, client sends the password:
+    cout << "Please enter your desired password << endl;
+    cin >> password;
+    int len2 = strlen(password.c_str())+1;
+    udpSend(udpSock, password.c_str(), len2, (struct sockaddr*)&udpSock, sizeof(struct sockaddr), "Could not send password");
+
+    //Ack that the user is now in:
+    //"Created a new account" or "username matched"
+    //or username does not match
+    udpRecv(udpSock, userResponseBuff2, 999, (struct sockaddr*)&client_socket, &addr_len, "Could not receive password information");
+    if(strcmp(userResponseBuff2, "Username does not match password given.") != 0) {
+      cout << userResponseBuff2 << endl;
+      exit(1);
+    }else{
+      cout << userResponseBuff2 << endl;
+    }
+    */
+    //Prompt user for operation state:
 
     exit(0);
 }
